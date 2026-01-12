23e412202d9982bb2457c0975dbf53a611fe8f5d
saacostam <73088662+saacostam@users.noreply.github.com>
2026-01-11 20:30:48 -0500

    feat(branding): update logo and page name

--- src/shared/components/logo/logo.tsx
+++ src/shared/components/logo/logo.tsx
@@ -1,22 +1,15 @@
 import { Flex, Text, ThemeIcon } from "@mantine/core";
-import { FireIcon } from "@/shared/icons";
+import { GamepadIcon } from "@/shared/icons";
 
 export function Logo() {
 	return (
 		<Flex align="center" gap="xs">
-			<ThemeIcon
-				variant="transparent"
-				size="lg"
-				color="var(--mantine-primary-color-5)"
-			>
-				<FireIcon />
+			<ThemeIcon variant="transparent" color="var(--mantine-primary-color-5)">
+				<GamepadIcon />
 			</ThemeIcon>
 			<Text fw="bold" size="xl">
-				<span style={{ color: "var(--mantine-primary-color-5)" }}>Clean</span>{" "}
-				React{" "}
-				<span style={{ color: "var(--mantine-primary-color-5)" }}>
-					Template
-				</span>
+				<span style={{ color: "var(--mantine-primary-color-5)" }}>Tic</span> Tac{" "}
+				<span style={{ color: "var(--mantine-primary-color-5)" }}>Toe</span>
 			</Text>
 		</Flex>
 	);
--- src/shared/icons/components/index.ts
+++ src/shared/icons/components/index.ts
@@ -1,5 +1,6 @@
 export * from "./cube-transparent.icon";
 export * from "./exclamation-circle.icon";
 export * from "./fire.icon";
+export * from "./gamepad.icon";
 export * from "./moon.icon";
 export * from "./sun.icon";
